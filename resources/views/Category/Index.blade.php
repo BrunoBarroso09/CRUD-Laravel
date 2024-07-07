1d34dd91ab8518a577aa82ba4a9602ba607cb532
Bruno Cruz <brunocruz@Brunos-MacBook-Air.local>
2024-07-07 19:17:37 +0100

    update index function to return view and new route for category and create view to list categories

---
+++ resources/views/Category/Index.blade.php
@@ -0,0 +1,38 @@
+@extends('layout.layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Categories list
+                            <a href="{{ url('category/create') }}" class="btn btn-primary float-end">Add Category</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-stiped table-bordered">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
